af63436f0df40fc9495901ecc317e4ee6154061a
TelurijevDioksid <tr54427@fer.hr>
2024-11-18 23:29:08 +0100

    add header for css

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
 
+	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css")
+		http.ServeFile(w, r, "./public/style.css")
+	})
+
 	log.Println("Listening on port 8080.")
 	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
