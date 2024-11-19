ba82a9a6edfa6ca05a8afc41f0366ca315f9fa4b
TelurijevDioksid <tr54427@fer.hr>
2024-11-19 01:04:04 +0100

    dodani komentari

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+// konfiguracijski objekt koji sadrži sve konstante
 let conf = {
     // general
     resScale: 0.1,
@@ -5,8 +6,8 @@ let conf = {
     ballColor: "blue",
     ballRadius: 8,
     ballSpeed: 5,
-    ballStartAngleMin: 0.7854,
-    ballStartAngleMax: 2.3562,
+    ballStartAngleMin: 0.7854, // 45 stupnjeva u rad
+    ballStartAngleMax: 2.3562, // 135 stupnjeva u rad
     // platform
     platformWidth: 100,
     platformHeight: 10,
@@ -14,15 +15,21 @@ let conf = {
     platformMoveSpeed: 10,
     // block
     blockScore: 1,
-    blocksNum: 10,
+    blocksNum: 10, // broj blokova
 }
-let gState = 0
-let cvs = null
-let ctx = null
-let gBall = null
-let gPlatform = null
-let gPltDir = 0
-let gScore = 0
+let gState = 0 // game state 0-Menu, 1-Game, 2-Game over, 3-Options, 4-Game win
+let cvs = null // canvas element
+let ctx = null // canvas 2d context
+let gBall = null // objekt lopta
+let gPlatform = null // objekt platforma
+let gPltDir = 0 // smjer platforme, 0 - stacionarno, 1 - lijevo, 2 - desno
+let gScore = 0 // score trenutne igre
+/*
+ * objekt blokovi
+ * arr - 2-dimenzijsko polje blokova
+ * broken - koliko blokova je vidljivo
+ * draw - funkcija za crtanje svih blokova iz arr i detekcija kolizije lopte i bloka
+*/
 const gBlocks = {
     arr: [],
     broken: 0,
@@ -43,25 +50,35 @@ const gBlocks = {
         }
     }
 }
+// lista objekata gumbova
 const gBtns = []
+/* objekt text boxeva odnosno user input
+ * writeIdx: trenutni text box u kojem se piše
+ * focusIdx: trenutni text box fokusirani
+ * arr: lista objekata text box
+ */
 const gTxtBoxes = {
     writeIdx: -1,
     focusIdx: -1,
     arr: []
 }
 
+// funkcija provjerava presjek točke i pravokutnika
 const intersectRect = (point, rect) => {
     return point.x >= rect.x && point.y >= rect.y &&
         point.x <= rect.x + rect.width &&
         point.y <= rect.y + rect.height
 }
 
+// funkcija provjerava presjek kruga i pravokutnika
 const intersectCircle = (circle, rect) => {
     return circle.x + circle.radius > rect.x && circle.x - circle.radius < rect.x + rect.width &&
         circle.y + circle.radius > rect.y && circle.y - circle.radius < rect.y + rect.height
 }
 
+// inicijalizira loptu
 const initBall = () => {
+    // nasumični odabir kuta u rasponu od 45 do 135 stupnjeva
     const rad = Math.random() * (conf.ballStartAngleMax - conf.ballStartAngleMin) + conf.ballStartAngleMin
     gBall = {
         x: cvs.width / 2,
@@ -79,6 +96,7 @@ const initBall = () => {
     }
 }
 
+// inicijalizira platformu
 const initPlatform = () => {
     gPlatform = {
         x: cvs.width / 2 - conf.platformWidth / 2,
@@ -86,10 +104,10 @@ const initPlatform = () => {
         width: conf.platformWidth,
         height: conf.platformHeight,
         draw() {
-            if (gPltDir === 1 && this.x > 20) {
+            if (gPltDir === 1 && this.x > 20) { // kretanje u lijevo
                 this.x -= conf.platformMoveSpeed
             }
-            if (gPltDir === 2 && this.x < cvs.width - this.width - 20) {
+            if (gPltDir === 2 && this.x < cvs.width - this.width - 20) { // kretanje u desno
                 this.x += conf.platformMoveSpeed
             }
             ctx.fillStyle = conf.platformColor
@@ -98,6 +116,7 @@ const initPlatform = () => {
     }
 }
 
+// inicijalizira blokove
 const initBlocks = () => {
     const blockWidth = Math.floor(0.1 * cvs.width)
     const blockHeight = 30
@@ -106,6 +125,7 @@ const initBlocks = () => {
     let blocksLeft = conf.blocksNum
 
     gBlocks.broken = conf.blocksNum
+    // popunjavanje arr 2d polja globalnog objekta sa objektima zasebnog bloka
     gBlocks.arr = new Array(rows)
     for (let i = 0; i < gBlocks.arr.length; i++) {
         gBlocks.arr[i] = new Array(blocksLeft > blocksPerRow ? blocksPerRow : blocksLeft)
@@ -121,7 +141,7 @@ const initBlocks = () => {
                 color: "#605678",
                 strokeColor: "#E07B39",
                 score: 1,
-                draw() {
+                draw() { // crta zaseban blok ukoliko nije razbijen
                     if (this.life === 0) {
                         return
                     }
@@ -137,6 +157,7 @@ const initBlocks = () => {
     }
 }
 
+// nalazi najveću rezoluciju kanvasa u omjeru 16/9 i vraća veličine kanvasa
 const findCsvSize = () => {
     const bodyX = document.body.offsetWidth
     const bodyY = document.body.offsetHeight
@@ -146,12 +167,13 @@ const findCsvSize = () => {
     return { width, height }
 }
 
+// postavlja i inicijalizira sve parametre
 const startUp = () => {
     const s = findCsvSize()
     cvs.width = s.width
     cvs.height = s.height
 
-    window.onresize = () => {
+    window.onresize = () => { // on resize handler postavlja novu rezoluciju kanvasa i ispisuje poruku
         const s = findCsvSize()
         cvs.width = s.width
         cvs.height = s.height
@@ -164,31 +186,32 @@ const startUp = () => {
         ctx.fillText("Window changed, please refresh page!", cvs.width / 2, cvs.height - (0.5 * cvs.height))
     }
 
+    // key down event ovisi o stanju igre
     cvs.onkeydown = (e) => {
         switch (gState) {
             case 0:
                 break
-            case 1:
-                if (gPltDir !== 1 && e.which === 37) {
-                    gPltDir = 1
+            case 1: // igra u tijeku
+                if (gPltDir !== 1 && e.which === 37) { // lijeva strelica
+                    gPltDir = 1 // smjer platforme u lijevo
                 }
-                else if (gPltDir !== 2 && e.which === 39) {
-                    gPltDir = 2
+                else if (gPltDir !== 2 && e.which === 39) { // desna strelica
+                    gPltDir = 2 // smjer platforme u desno
                 }
-                else if (gPltDir !== 0) {
-                    gPltDir = 0
+                else if (gPltDir !== 0) { // platforma miruje
+                    gPltDir = 0 // postavi da platforma miruje
                 }
                 break
-            case 2:
+            case 2: // game over, pritisak na bilo koju tipku vraća na meni
                 gState = 0
                 drawMenu()
                 break
-            case 3:
+            case 3: // podešavanje parametara, ukoliko je tipka znamenka od 0-9
                 if (e.which >= 48 && e.which <= 57 && gTxtBoxes.focusIdx >= 0) {
                     gTxtBoxes.arr[gTxtBoxes.focusIdx].txt = gTxtBoxes.arr[gTxtBoxes.focusIdx].txt + e.key
                 }
                 break
-            case 4:
+            case 4: // game win stanje, pritisak na bilo koju tipku prebaci na meni
                 gState = 0
                 drawMenu()
                 break
@@ -198,7 +221,7 @@ const startUp = () => {
     }
 
     cvs.onkeyup = (e) => {
-        if (gPltDir !== 0) {
+        if (gPltDir !== 0) { // platforma miruje
             gPltDir = 0
         }
     }
@@ -209,6 +232,7 @@ const startUp = () => {
             y: e.clientY - ((document.body.clientHeight - cvs.height) / 2),
         }
 
+        // postavi text box spreman za primanje user inputa ovisno poziciji klika miša
         for (let i = 0; i < gTxtBoxes.arr.length; i++) {
             if (gTxtBoxes.arr[i].state === gState && intersectRect(mousePos, gTxtBoxes.arr[i])) {
                 if (gTxtBoxes.writeIdx >= 0) {
@@ -219,21 +243,16 @@ const startUp = () => {
             }
         }
 
+        // prosljedi klik event na odgovarajući gumb
         for (let i = 0; i < gBtns.length; i++) {
             if (gBtns[i].state === gState && intersectRect(mousePos, gBtns[i])) {
                 gBtns[i].onClick()
                 return
             }
         }
-
-        for (let i = 0; i < gTxtBoxes.arr.length; i++) {
-            if (gTxtBoxes.arr[i].state === gState && intersectRect(mousePos, gTxtBoxes.arr[i])) {
-                gTxtBoxes.arr[i].write = true
-                return
-            }
-        }
     }
 
+    // provjerava ako se miš nalazi iznad gumba ili text boxa i postavlja cursor style ovisno o slučaju
     cvs.onmousemove = (e) => {
         const mousePos = {
             x: e.clientX - ((document.body.clientWidth - cvs.width) / 2),
@@ -292,6 +311,11 @@ const startUp = () => {
     })
 }
 
+/* dodaje novi text box u globalnu listu
+ * state - stanje igre, label - text prije user inputa, preTxt - input vrijednost
+ * maxlen - max duljina inputa, x - x kordinata, y - y kordinata, onChande funkcija
+ * kada se upiše neka vrijednost
+*/
 const initTxtBox = (state, label, preTxt, maxlen, x, y, onChange) => {
     ctx.font = "30px Arial"
     const wBox = ctx.measureText(label).width
@@ -334,6 +358,9 @@ const initTxtBox = (state, label, preTxt, maxlen, x, y, onChange) => {
     })
 }
 
+/* dodaje novi text gumb u listu gumbova
+ * state - stanje igre, str - text gumba, x - x kordinata, y - y kordinata, fn - funkcija kada se klikne na gumb
+*/
 const initBtn = (state, str, x, y, fn) => {
     ctx.font = "30px Arial"
     const wTxt = ctx.measureText(str).width
@@ -364,9 +391,12 @@ const initBtn = (state, str, x, y, fn) => {
     })
 }
 
+// crta menu 
 const drawMenu = () => {
+    // obriši cijeli kanvas
     ctx.clearRect(0, 0, cvs.width, cvs.height)
 
+    // dohvati vrijednosti iz local storage-a
     const prevScr = localStorage.getItem("prev") || "0"
     const highScr = localStorage.getItem("high") || "0"
 
@@ -376,7 +406,7 @@ const drawMenu = () => {
     ctx.fillText("Menu", cvs.width / 2, cvs.height - (0.8 * cvs.height))
 
     ctx.font = "20px Arial"
-    gBtns.forEach(b => b.draw())
+    gBtns.forEach(b => b.draw()) // crtaj svaki gumb koji spada u ovo stanje
 
     ctx.fillText(`High score: ${highScr}`, cvs.width / 2, cvs.height - (0.25 * cvs.height))
     ctx.fillText(`Prev score: ${prevScr}`, cvs.width / 2, cvs.height - (0.2 * cvs.height))
@@ -384,6 +414,7 @@ const drawMenu = () => {
     raf = window.requestAnimationFrame(drawMenu)
 }
 
+// nacrtaj postavke meni
 const drawOptions = () => {
     ctx.clearRect(0, 0, cvs.width, cvs.height)
 
@@ -393,21 +424,23 @@ const drawOptions = () => {
     ctx.fillText("Options", cvs.width / 2, cvs.height - (0.8 * cvs.height))
 
     ctx.font = "20px Arial"
-    gTxtBoxes.arr.forEach(b => b.draw())
-    gBtns.forEach(b => b.draw())
+    gTxtBoxes.arr.forEach(b => b.draw()) // crtaj sve txt boxeve koje spadaju u ovo stanje
+    gBtns.forEach(b => b.draw()) // crtaj svaki gumb koji spada u ovo stanje
 
     raf = window.requestAnimationFrame(drawOptions)
 }
 
+// nacrtaj igru
 const drawGame = () => {
     ctx.clearRect(0, 0, cvs.width, cvs.height)
 
-    gBlocks.draw()
-    gPlatform.draw()
-    gBall.draw()
-    gBall.x += gBall.vx
-    gBall.y += gBall.vy
+    gBlocks.draw() // nacrtaj sve blokove
+    gPlatform.draw() // nacrtaj platformu
+    gBall.draw() // nacrtaj loptu
+    gBall.x += gBall.vx // promjeni x kordinatu lopte
+    gBall.y += gBall.vy // promjeni y kordinatu lopte
 
+    // prikaži score u desnom kutu igre
     const high = localStorage.getItem("high") || "0"
     ctx.font = "12px Arial"
     ctx.fillStyle = "black"
@@ -416,16 +449,17 @@ const drawGame = () => {
     ctx.fillText(`High score: ${high}`, cvs.width - 10, 30)
     ctx.textAlign = "center"
 
-    if (gBall.y > cvs.height) {
+    if (gBall.y > cvs.height) { // ako lopta padne iza platforme
         gState = 2
         drawGameOver()
         return
     }
-    if (gBlocks.broken === 0) {
+    if (gBlocks.broken === 0) { // ako su svi blokovi razbijeni
         gState = 4
         drawGameWin()
         return
     }
+    // dalje slučajevi lopta odbijena od platforme, bočnih zidova i gornjeg zida
     if (intersectCircle(gBall, gPlatform)) {
         gBall.vy = gBall.vy > 0 ? -gBall.vy : gBall.vy
     }
@@ -439,9 +473,11 @@ const drawGame = () => {
     raf = window.requestAnimationFrame(drawGame)
 }
 
+// nacrtaj game win stanje
 const drawGameWin = () => {
-    window.cancelAnimationFrame(raf)
+    window.cancelAnimationFrame(raf) // prekini animaciju
 
+    // resetiraj samo potrebne parametre lopte, platforme i blokova
     const rad = Math.random() * (conf.ballStartAngleMax - conf.ballStartAngleMin) + conf.ballStartAngleMin
     gBall.vx = conf.ballSpeed * Math.cos(rad)
     gBall.vy = -conf.ballSpeed * Math.sin(rad)
@@ -458,11 +494,13 @@ const drawGameWin = () => {
     })
     gBlocks.broken = conf.blocksNum
 
+    // spremi score
     localStorage.setItem("prev", gScore)
     if (gScore > localStorage.getItem("high")) {
         localStorage.setItem("high", gScore)
     }
 
+    // očisti kanvas i ispiši poruke
     ctx.clearRect(0, 0, cvs.width, cvs.height)
     ctx.font = "40px Arial"
     ctx.fillStyle = "green"
@@ -478,9 +516,11 @@ const drawGameWin = () => {
     ctx.fillText(`Score: ${gScore}`, cvs.width / 2, cvs.height - (0.4 * cvs.height))
 }
 
+// nacrtaj game over stanje
 const drawGameOver = () => {
     window.cancelAnimationFrame(raf)
 
+    // resetiraj samo potrebne parametre lopte, platforme i blokova
     const rad = Math.random() * (conf.ballStartAngleMax - conf.ballStartAngleMin) + conf.ballStartAngleMin
     gBall.vx = conf.ballSpeed * Math.cos(rad)
     gBall.vy = -conf.ballSpeed * Math.sin(rad)
@@ -497,11 +537,13 @@ const drawGameOver = () => {
     })
     gBlocks.broken = conf.blocksNum
 
+    // spremi novi rezultat
     localStorage.setItem("prev", gScore)
     if (gScore > localStorage.getItem("high")) {
         localStorage.setItem("high", gScore)
     }
 
+    // očisti kanvas i ispiši poruke
     ctx.clearRect(0, 0, cvs.width, cvs.height)
     ctx.font = "40px Arial"
     ctx.fillStyle = "red"
@@ -517,10 +559,13 @@ const drawGameOver = () => {
     ctx.fillText(`Score: ${gScore}`, cvs.width / 2, cvs.height - (0.4 * cvs.height))
 }
 
+// kada se učita stranica
 window.onload = () => {
+    // postavi cvs na element kanvasa
     cvs = document.getElementById("cvs")
+    // postavi ctx na 2d kontekst
     ctx = cvs.getContext("2d")
 
-    startUp()
-    drawMenu()
+    startUp() // inicijalizacija svih potrebnih objekata i event listenera
+    drawMenu() // prikaži početno stanje
 }
